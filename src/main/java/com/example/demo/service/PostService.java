aba355f0cb237fae6802e0a7d094b2e48c9043c1
Igor Dias <igorverissimo02@hotmail.com>
2024-05-03 19:04:19 -0300

    feat: get post by id

---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Post;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post post = repo.findById(id).orElse(null);
+		
+		if(post == null) {
+			throw new ObjectNotFoundException("Object not found");
+		}
+		return post;
+	}
+	
+}
