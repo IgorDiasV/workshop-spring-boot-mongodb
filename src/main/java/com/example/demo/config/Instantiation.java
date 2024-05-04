7060039a7f364cdc761b39da14dc3ad6a69015de
Igor Dias <igorverissimo02@hotmail.com>
2024-05-04 08:48:39 -0300

    feat: add coments

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.AuthorDTO;
+import com.example.demo.dto.CommentDTO;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viamgem!", sdf.parse("04/05/2024"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Boa viamgem2!", sdf.parse("01/05/2024"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Boa viamgem3!", sdf.parse("08/05/2024"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
